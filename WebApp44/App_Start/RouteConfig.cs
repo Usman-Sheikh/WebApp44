48400e0347dc4e89623ea92da871e40068a7b5eb
Usman Sheikh <sh.usmanikram@hotmail.com>
2017-02-24 16:31:58 +0500

    RouteModified

--- WebApp44/App_Start/RouteConfig.cs
+++ WebApp44/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WebApp44
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Default", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
